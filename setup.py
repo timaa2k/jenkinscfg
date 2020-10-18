2c6ad1ba9dd15f6438c2db2930df368df3b42057
Tim Weidner <timaa2k@gmail.com>
2020-10-18 23:30:53 +0200

    getting ready for publishing

--- setup.py
+++ setup.py
@@ -1,32 +1,23 @@
 import pathlib
+import pkg_resources
 import setuptools
 from typing import List
 
 
-def _get_dependencies(requirements_file: pathlib.Path) -> List[str]:
-    """
-    Return requirements from a requirements file.
-    This expects a requirements file with no ``--find-links`` lines.
-    """
-    lines = requirements_file.read_text().strip().split('\n')
-    print(lines)
-    return [line for line in lines if not line.startswith('#')]
-
-
-INSTALL_REQUIRES = _get_dependencies(
-    requirements_file=pathlib.Path('requirements.txt'),
-)
-
-DEV_REQUIRES = _get_dependencies(
-    requirements_file=pathlib.Path('dev-requirements.txt'),
-)
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    INSTALL_REQUIRES = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
 
 LONG_DESCRIPTION = pathlib.Path('README.rst').read_text()
 
 setuptools.setup(
     name='jenkinscfg',
-    version='0.1.0',
+    version='0.1.0a1',
     author='Tim Weidner',
+    author_email='timaa2k@gmail.com',
     description='Declarative Jenkins Jobs Configuration',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
@@ -36,18 +27,22 @@ setuptools.setup(
     package_dir={'': 'src'},
     zip_safe=False,
     install_requires=INSTALL_REQUIRES,
-    extras_require={
-        'dev': DEV_REQUIRES,
-    },
     classifiers=[
         'Development Status :: 3 - Alpha',
+        'Environment :: Console',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: GPLv3 License',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
     ],
     dependency_links=[],
+    entry_points={
+        'console_scripts': [
+            'jenkinscfg=jenkinscfg.cli:cli',
+        ],
+    }
 )
